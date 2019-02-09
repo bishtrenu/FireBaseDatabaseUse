bfb64828e18bb6de240c762cea3fa0365c6150dd
renu <bishtrenu243@gmail.com>
2019-02-09 21:42:34 +0530

    app completed

--- app/src/main/java/com/example/renu/fireusersystem/MainActivity.java
+++ app/src/main/java/com/example/renu/fireusersystem/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.renu.fireusersystem;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 FirebaseDatabase db=FirebaseDatabase.getInstance();
@@ -26,7 +32,23 @@ DatabaseReference userRef=rootref.child("Users");
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+            String userid=uname.getText().toString().trim();
+            String username=name.getText().toString().trim();
+            String useremail=email.getText().toString().trim();
+                HashMap<String,String> usermap=new HashMap<String, String>();
+                usermap.put("id",userid);
+                usermap.put("name",username);
+                usermap.put("email",useremail);
+                userRef.push().setValue(usermap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this,"Success",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
             }
         });
     }
